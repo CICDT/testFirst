8f93ec8c8fe6eb99e6e31f5facbd34a19b9baf54
CICDT <lindbjos@gmail.com>
2025-05-09 00:51:13 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main{
         System.out.println("Output from the java program");
         System.out.println(new Date());
         System.out.println("Hello");
+        System.out.println("Hello world");
 
     }
 }
